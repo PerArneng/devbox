e89cdc1032c604cb3feb5f410fed386b0f759273
Per Arneng <per.arneng@scalebit.com>
2015-03-28 22:43:10 +0100

    formatting

--- install_devbox.sh
+++ install_devbox.sh
@@ -3,61 +3,61 @@
 TEMP_INVENTORY_FILE=__temp_inv_file_remove
 
 function log {
-	echo "$1"
+    echo "$1"
 }
 
 function print_help {
-	log "Install Devbox 0.6"
-	log "usage: sealdockercihelper [-u username] [-r remote_host] [-t] [-h]"
-	log "OPTIONS:"
-	log "   -u <username>        remote username"
-	log "   -r <remote_host>     the hostname/ip to the remote machine"
-	log "   -t                   just test the connection to the remote host"
-	log "   -h                   print this help"
-	log ""
-	log "EXAMPLE:"
-	log "   $ install_devbox -u rupert -r 192.168.5.78"
+    log "Install Devbox 0.6"
+    log "usage: sealdockercihelper [-u username] [-r remote_host] [-t] [-h]"
+    log "OPTIONS:"
+    log "   -u <username>        remote username"
+    log "   -r <remote_host>     the hostname/ip to the remote machine"
+    log "   -t                   just test the connection to the remote host"
+    log "   -h                   print this help"
+    log ""
+    log "EXAMPLE:"
+    log "   $ install_devbox -u rupert -r 192.168.5.78"
 }
 
 function check_arg {
-	if [[ -z $2 ]]
-	then
-		log "$1 is missing"
-		print_help
-		exit 1
-	fi
+    if [[ -z $2 ]]
+    then
+        log "$1 is missing"
+        print_help
+        exit 1
+    fi
 }
 
 function test_con {
-	ansible --user=$1 --ask-pass --inventory-file=$TEMP_INVENTORY_FILE all -a "/bin/echo ok"
+    ansible --user=$1 --ask-pass --inventory-file=$TEMP_INVENTORY_FILE all -a "/bin/echo ok"
 }
 
 function cleanup {
-	rm $TEMP_INVENTORY_FILE
+    rm $TEMP_INVENTORY_FILE
 }
 
 while getopts "u:r:th" opt; do
-  	case $opt in
-	  	h)
-			print_help
-			exit	
-			;;
+    case $opt in
+        h)
+            print_help
+            exit    
+            ;;
 
-	    u) username=$OPTARG;;
-	    r) remote_host=$OPTARG;;
-		t) test_connection=true;;
+        u) username=$OPTARG;;
+        r) remote_host=$OPTARG;;
+        t) test_connection=true;;
 
-	    \?)
-	    	log "invalid option: -$OPTARG" >&2
-	    	print_help
-	    	exit 1
-	    	;;
-	    :)
-	    	log "option -$OPTARG requires an argument." >&2
-	    	print_help
-	    	exit 1
-	    	;;
-	esac
+        \?)
+            log "invalid option: -$OPTARG" >&2
+            print_help
+            exit 1
+            ;;
+        :)
+            log "option -$OPTARG requires an argument." >&2
+            print_help
+            exit 1
+            ;;
+    esac
 done
 
 check_arg "username" $username
@@ -73,10 +73,10 @@ echo $remote_host > $TEMP_INVENTORY_FILE
 
 if [[ -n $test_connection ]]
 then
-	log "testing connection: $username@$remote_host"
-	test_con $username
-	cleanup
-	exit 0
+    log "testing connection: $username@$remote_host"
+    test_con $username
+    cleanup
+    exit 0
 fi
 
 log "running playbooks"
